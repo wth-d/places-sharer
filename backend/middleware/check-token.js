7823c332c36bd0de958f4ae66e7330c6b678a2f7
Wei <111629334+wth-d@users.noreply.github.com>
2023-08-12 13:13:17 -0400

    handle OPTIONS request in backend; 181

--- backend/middleware/check-token.js
+++ backend/middleware/check-token.js
@@ -5,6 +5,11 @@ const HttpError = require("../models/http-error");
 // logic of a middleware function for validating the token in a request
 
 module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") { // handle OPTIONS requests
+    next();
+    return;
+  }
+
   try {
     // req.headers.authorization could be undefined if the authorization header isn't added
     const token = req.headers.authorization.split(" ")[1]; // the header will look like - (Authorization: 'Bearer TOKEN')
@@ -17,7 +22,7 @@ module.exports = (req, res, next) => {
     req.userData = { userId: decodedPayload.userId };
     next(); // if no error above, then the token is successfully verified;
   } catch (error) {
-    console.log("Possible errors: Token not found in request OR token is invalid.");
+    console.log("Possible errors: Token not attached to request OR attached token is invalid.");
     const err = new HttpError("Authentication failed (in check-token.js)!", 401);
     next(err);
     return;
